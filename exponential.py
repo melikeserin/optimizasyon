7070097b68520835dd672ea309cf0f044a1bfa4c
melikeserin <139703192+melikeserin@users.noreply.github.com>
2025-05-21 22:16:58 +0300

    Update exponential.py

--- exponential.py
+++ exponential.py
@@ -14,3 +14,10 @@ def error(xk,ti,yi):
   return np.array(yi) - np.array(yhat)
 
 def findJacobien(traininginput,x):
+  numofdata = llen(traininginput)
+  J = np.matrix(np.zeros((numofdata,2)))
+  for i in range(0, numofdata):
+    J[i,0] = -math.exp(x[1]*traininginput[i])
+    J[i,1] = -x[0]*traininginput[i]*math.exp(x[1]*traininginput[i])
+    return J
+  
