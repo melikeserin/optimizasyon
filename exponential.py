ae339a82a50aa1a460a6756195e86ba439867580
melikeserin <139703192+melikeserin@users.noreply.github.com>
2025-05-21 22:12:43 +0300

    Update exponential.py

--- exponential.py
+++ exponential.py
@@ -1,1 +1,16 @@
+import numpy as np
+import math
+from dataset5 import ti,yi
 
+def exponentialIO(t,x):
+  yhat = []
+  for ti in t:
+    toplam = x[0]*math.exp(x[1]*ti)
+    yhat.append(toplam)
+  return yhat
+
+def error(xk,ti,yi):
+  yhat = exponentialIO(ti,xk)
+  return np.array(yi) - np.array(yhat)
+
+def findJacobien(traininginput,x):
